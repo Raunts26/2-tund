8ab3ff2be7c72042fbf63947c92af0c61901ca17
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-09-07 15:33:46 +0300

    lisatud kontroll emailile ja paroolile

--- userform.php
+++ userform.php
@@ -1,10 +1,29 @@
 
 <?php
 
-	// user_form.php
+	$email_error = "";
+	$password_error = "";
+	/* user_form.php
+	Jutumarkide vahele input elemendi NAME
 	
+	echo $_POST["email"];
+	echo $_POST["password"];*/
 
-
+	//Kontrolli ss kui kasutaja vajutab submit
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//Kontrolli kasutaja e-posti ja parooli, et see poleks tühi
+		if (empty($_POST["email"])) {
+			$email_error = "Teie e-posti lünk on tühi";
+		
+		}	
+		if (empty($_POST["password"])) {
+			$password_error = "Teie parooli lünk on tühi";
+		
+		}	
+		
+	}
+	
 
 
 
@@ -16,11 +35,13 @@
 		<title>UCP</title>
 	</head>
 	<body>
-		
+		<form action="userform.php" method="post">
 		<h2>Logi sisse</h2>
-		<input type="email" placeholder="E-post"><br><br>
-		<input type="password" placeholder="Parool"><br><br>
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
+		
 		<input type="submit" value="Logi sisse">
+		</form>
 		
 		<h2>Registreeru</h2>
 		
